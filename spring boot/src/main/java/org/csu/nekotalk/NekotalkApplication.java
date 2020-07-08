90aa3f56724dbecb7d5271dd97de76bb4283a6d0
YaxunYang <44467262+Yaxun-Yang@users.noreply.github.com>
2020-07-08 11:00:41 +0800

    init

---
+++ spring boot/src/main/java/org/csu/nekotalk/NekotalkApplication.java
@@ -0,0 +1,13 @@
+package org.csu.nekotalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekotalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NekotalkApplication.class, args);
+    }
+
+}
---
+++ spring boot/src/test/java/org/csu/nekotalk/NekotalkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.nekotalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NekotalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
